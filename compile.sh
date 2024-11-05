13438c1153e3fc6050c589a8427107d3de575dda
taishi-n <taishi.m.and.p@gmail.com>
2024-11-05 20:30:09 +0900

    fix: filename

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 pandoc --include-in-header=header.tex --pdf-engine=lualatex -t beamer slide.md -o slide.pdf
-pandoc --include-in-header=header.tex --include-in-header=note.tex --pdf-engine=lualatex -t beamer slide.md -o slide_note.pdf
+pandoc --include-in-header=header.tex --include-in-header=shownote.tex --pdf-engine=lualatex -t beamer slide.md -o slide_note.pdf
